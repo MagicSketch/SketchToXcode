92e880f52502832f5cae4b0e4aa019f85de47f1e
James Tang <j@jamztang.com>
2016-12-30 14:27:09 +0800

    Initial Commit

---
+++ SketchToXcode/SketchToXcode.h
@@ -0,0 +1,19 @@
+//
+//  SketchToXcode.h
+//  SketchToXcode
+//
+//  Created by James Tang on 30/12/2016.
+//  Copyright © 2016 MagicSketch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SketchToXcode.
+FOUNDATION_EXPORT double SketchToXcodeVersionNumber;
+
+//! Project version string for SketchToXcode.
+FOUNDATION_EXPORT const unsigned char SketchToXcodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SketchToXcode/PublicHeader.h>
+
+
---
+++ SketchToXcodeTests/SketchToXcodeTests.m
@@ -0,0 +1,39 @@
+//
+//  SketchToXcodeTests.m
+//  SketchToXcodeTests
+//
+//  Created by James Tang on 30/12/2016.
+//  Copyright © 2016 MagicSketch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SketchToXcodeTests : XCTestCase
+
+@end
+
+@implementation SketchToXcodeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
